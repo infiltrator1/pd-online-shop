2eb22201c260fab11497df345e34809b55e41672
Aleksander Petrov <apetrovdev@gmail.com>
2022-06-08 23:24:49 +0300

    fix for carousel images

--- src/pages/product/CarouselProduct.js
+++ src/pages/product/CarouselProduct.js
@@ -1,122 +1,32 @@
-import { Col, Carousel } from "react-bootstrap";
+import {Col, Carousel, Form} from "react-bootstrap";
 import {useParams} from "react-router-dom";
 import {useGetCloth} from "../../graphql/useRequest"
 
 function CarouselProduct() {
 
-  const { id } = useParams();
-  const { data } = useGetCloth(id);
+    const {id} = useParams();
+    const {data} = useGetCloth(id);
 
-  //const image3 = data.clothesOne.imageList[3];
-  //const image4 = data.clothesOne.imageList[4];
+    return (
 
-  //if ( image3 === true) {
-  //    console.log(image3)
-  //} 
-
-
-    return(   
-
-      <Col sm={7}>
-        <Carousel
-            variant="dark"
-            className="flex-start mt-1">
-              <Carousel.Item>
-              <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src= {data.clothesOne.imageList[0]}
-                   alt="First slide"
-                />      
-              </Carousel.Item>
-              <Carousel.Item>
-              <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src= {data.clothesOne.imageList[1]}
-                   alt="First slide"
-                />      
-              </Carousel.Item>
-              <Carousel.Item>
-              <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src= {data.clothesOne.imageList[2]}
-                   alt="First slide"
-                />      
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                     className="d-block mx-auto"
-                     width={500}
-                     src= {data.clothesOne.imageList[3]}
-                     alt="First slide"
-                  />      
-              </Carousel.Item>
-              <Carousel.Item>
-              <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src= {data.clothesOne.imageList[4]}
-                   alt="First slide"
-                />
-              </Carousel.Item>
-        </Carousel>
-      </Col>
-
-    );
-    }
-
-      /*    
-          <Col sm={7}>
-            <Carousel 
+        <Col sm={7}>
+            <Carousel
                 variant="dark"
                 className="flex-start mt-1">
-              <Carousel.Item>
-                <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src="https://static.bershka.net/4/photos2/2022/V/0/2/p/0618/019/800/0618019800_1_1_3.jpg?t=1640105351049"
-                   alt="First slide"
-                />      
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block mx-auto "
-                  width={500}
-                  src="https://static.bershka.net/4/photos2/2022/V/0/2/p/0618/019/800/0618019800_2_1_1.jpg?t=1640105351049"
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                   className="d-block mx-auto"
-                   width={500}
-                   src="https://static.bershka.net/4/photos2/2022/V/0/2/p/0618/019/800/0618019800_2_2_1.jpg?t=1640105351049"
-                   alt="Third slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block mx-auto"
-                  width={500}
-                  src="https://static.bershka.net/4/photos2/2022/V/0/2/p/0618/019/800/0618019800_2_3_1.jpg?t=1640105351049"
-                  alt="Fourth slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block mx-auto"
-                  width={500}
-                  src="https://static.bershka.net/4/photos2/2022/V/0/2/p/0618/019/800/0618019800_2_5_1.jpg?t=1640105351049"
-                  alt="Fourth slide"
-                />
-              </Carousel.Item>
+                {data && data.clothesOne.imageList.map((image, index) => (
+                    <Carousel.Item key={index}>
+                        <img
+                            className="d-block mx-auto"
+                            width={500}
+                            src={image}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                ))}
             </Carousel>
-          </Col>
-        );
-}
+        </Col>
 
-*/
+    );
+}
 
 export default CarouselProduct;
